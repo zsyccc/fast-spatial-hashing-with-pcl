3740077a3cfdc88f05989a3f10c0312748423b2d
zsyccc <zsyc@njfu.edu.cn>
2019-08-06 21:22:28 +0800

    add debug info

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #define PCL_NO_PRECOMPILE
+
 #include <pcl/console/parse.h>
 #include <pcl/console/print.h>
 #include <pcl/visualization/pcl_visualizer.h>
@@ -12,38 +13,35 @@
 using namespace std;
 using namespace pcl;
 const int default_number_samples = 100000;
-const float default_leaf_size = 0.001f;
+const float default_leaf_size = 0.01f;
 
-pcl::PointCloud<pcl::Normal>::Ptr get_normals(
-    const pcl::PointCloud<pcl::PointXYZ>::Ptr& cloud) {
+pcl::PointCloud<pcl::Normal>::Ptr get_normals(const pcl::PointCloud<pcl::PointXYZ>::Ptr &cloud) {
     pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> ne;
-    pcl::search::KdTree<pcl::PointXYZ>::Ptr tree(
-        new pcl::search::KdTree<pcl::PointXYZ>);
+    pcl::search::KdTree<pcl::PointXYZ>::Ptr tree(new pcl::search::KdTree<pcl::PointXYZ>);
     ne.setInputCloud(cloud);
     ne.setSearchMethod(tree);
     ne.setKSearch(50);
-    pcl::PointCloud<pcl::Normal>::Ptr cloud_normals(
-        new pcl::PointCloud<pcl::Normal>);
+    pcl::PointCloud<pcl::Normal>::Ptr cloud_normals(new pcl::PointCloud<pcl::Normal>);
     ne.compute(*cloud_normals);
     return cloud_normals;
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
     if (argc < 2) {
         console::print_error("no enough param\n");
         return (-1);
     }
 
     std::vector<int> obj_file_indices =
-        console::parse_file_extension_argument(argc, argv, ".obj");
+            console::parse_file_extension_argument(argc, argv, ".obj");
 
     if (obj_file_indices.size() != 1) {
         console::print_error("Need a single input OBJ file to continue.\n");
         return (-1);
     }
-    const char* filename = argv[obj_file_indices[0]];
+    const char *filename = argv[obj_file_indices[0]];
     PointCloud<PointXYZ>::Ptr voxel_cloud =
-        mesh_sampling(filename, default_number_samples, default_leaf_size);
+            mesh_sampling(filename, default_number_samples, default_leaf_size);
 
     PointCloud<Normal>::Ptr normals = get_normals(voxel_cloud);
     PointCloud<PointNormal>::Ptr pNormal(new PointCloud<PointNormal>);
@@ -52,12 +50,6 @@ int main(int argc, char** argv) {
     //                                     default_leaf_size);
 
     // clustering
-    // VSA vsa;
-    // vsa.setInputCloud<pcl::PointNormal>(pNormal);
-    // vsa.setMetricOption(2);
-    // vsa.setEps(0.01);
-    // vsa.setK(6);
-    // auto res = vsa.compute();
 
     // std::default_random_engine generator;
     // std::uniform_int_distribution<int> color(0, 255);
@@ -87,7 +79,7 @@ int main(int argc, char** argv) {
     using PshPointT = Psh::data_t;
     pcl::PointCloud<PshPointT>::Ptr psh_cloud(new pcl::PointCloud<PshPointT>);
     pcl::copyPointCloud(*pNormal, *psh_cloud);
-    for (auto& it : *psh_cloud) {
+    for (auto &it : *psh_cloud) {
         it.costomData = true;
     }
     Psh map(psh_cloud);
--- psh.hpp
+++ psh.hpp
@@ -14,28 +14,51 @@
 #include <vsa.hpp>
 #include <iostream>
 
-template <typename T>
+#define PSH_DEBUG
+
+template<typename T>
 struct PointNormalTData {
     PCL_ADD_POINT4D;
     PCL_ADD_NORMAL4D;
     T costomData;
+
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+} EIGEN_ALIGN16;
+
+template<typename T>
+struct PointTData {
+    PCL_ADD_POINT4D;
+    T costomData;
+
     EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 } EIGEN_ALIGN16;
 
 POINT_CLOUD_REGISTER_POINT_STRUCT(
-    PointNormalTData<bool>,
-    (float, x, x)(float, y, y)(float, z, z)(float, normal_x, normal_x)(
-        float, normal_y, normal_y)(float, normal_z, normal_z))
+        PointNormalTData<bool>,
+        (float, x, x)
+                (float, y, y)
+                (float, z, z)
+                (float, normal_x, normal_x)
+                (float, normal_y, normal_y)
+                (float, normal_z, normal_z)
+)
+
+POINT_CLOUD_REGISTER_POINT_STRUCT(
+        PointTData<bool>,
+        (float, x, x)
+                (float, y, y)
+                (float, z, z)
+)
 
 namespace psh {
 
-    template <typename T>
+    template<typename T>
     class psh {
     public:
         using data_t = PointNormalTData<T>;
 
-        psh(const typename pcl::PointCloud<data_t>::Ptr& in) {
-            const int k = 100;
+        psh(const typename pcl::PointCloud<data_t>::Ptr &in) {
+            const int k = 6;
             VSA vsa;
             vsa.setInputCloud<data_t>(in);
             vsa.setMetricOption(2);
@@ -44,33 +67,31 @@ namespace psh {
             auto res = vsa.compute();
             auto proxies = vsa.getProxies();
 
-            std::vector<typename pcl::PointCloud<data_t>::Ptr>
-                transformed_clouds;
+            std::vector<typename pcl::PointCloud<data_t>::Ptr> transformed_clouds;
             std::generate_n(std::back_inserter(transformed_clouds), k, []() {
-                typename pcl::PointCloud<data_t>::Ptr ret(
-                    new pcl::PointCloud<data_t>);
+                typename pcl::PointCloud<data_t>::Ptr ret(new pcl::PointCloud<data_t>);
                 return ret;
             });
 
-            for (auto it : res) {
+#ifdef PSH_DEBUG
+            // debug: print the size of each bucket
+            for (const auto &it : res) {
                 std::cout << it.size() << ' ';
             }
             std::cout << std::endl;
-
-            pcl::visualization::PCLVisualizer viewer("PSH_rotate");
-            pcl::visualization::PCLVisualizer viewer2("VSA");
-            std::default_random_engine generator;
-            std::uniform_int_distribution random(0, 255);
+            // end debug
+#endif
             for (int bucket = 0; bucket < k; bucket++) {
-                typename pcl::PointCloud<data_t>::Ptr source(
-                    new typename pcl::PointCloud<data_t>);
+                // load source cloud
+                typename pcl::PointCloud<data_t>::Ptr source(new typename pcl::PointCloud<data_t>);
                 source->points.reserve(res[bucket].size());
-                for (const auto& point_idx : res[bucket]) {
+                for (const auto &point_idx : res[bucket]) {
                     source->points.push_back(in->points[point_idx]);
                 }
-                const auto& proxy = proxies[bucket];
-                Eigen::Vector3f N(proxy.normal_x, proxy.normal_y,
-                                  proxy.normal_z);
+                // use proxy
+                const auto &proxy = proxies[bucket];
+                // rotation
+                Eigen::Vector3f N(proxy.normal_x, proxy.normal_y, proxy.normal_z);
                 Eigen::Vector3f X(0.0f, 0.0f, 1.0f);
                 Eigen::Vector3f rotation_axis = N.cross(X);
                 float rotation_angle = std::acos(N.dot(X) / N.norm());
@@ -78,44 +99,55 @@ namespace psh {
                 Eigen::Matrix3f rotation_matrix = angle_axis.matrix();
                 Eigen::Matrix4f transform_matrix = Eigen::Matrix4f::Zero();
                 transform_matrix(3, 3) = 1;
-                transform_matrix(2, 3) = (float)bucket * 0.1f;
+#ifdef PSH_DEBUG
+                // debug: adjust the z axis
+                transform_matrix(2, 3) = (float) bucket * 0.1f;
+                // end debug
+#endif
                 for (int i = 0; i < 3; i++) {
                     for (int j = 0; j < 3; j++) {
-                        if (std::fabs(rotation_matrix(i, j)) < 1e-5)
-                            rotation_matrix(i, j) = 0;
                         transform_matrix(i, j) = rotation_matrix(i, j);
                     }
                 }
+                pcl::transformPointCloud(*source, *transformed_clouds[bucket], transform_matrix);
+            }
 
-                // std::cout << " -- " << std::endl;
-                // std::cout << X << "\n" << std::endl;
-                // std::cout << N << "\n" << std::endl;
-                // std::cout << (rotation_matrix * N.normalized()).normalized()
-                //           << "\n"
-                //           << std::endl;
-                // std::cout << rotation_matrix << "\n" << std::endl;
+#ifdef PSH_DEBUG
+            // debug: draw
+            pcl::visualization::PCLVisualizer viewer("PSH_rotate");
+            pcl::visualization::PCLVisualizer viewer2("VSA");
+
+            std::default_random_engine generator;
+            std::uniform_int_distribution random(0, 255);
+
+            for (int bucket = 0; bucket < k; bucket++) {
+                typename pcl::PointCloud<data_t>::Ptr source(
+                        new typename pcl::PointCloud<data_t>);
+                source->points.reserve(res[bucket].size());
+                for (const auto &point_idx : res[bucket]) {
+                    source->points.push_back(in->points[point_idx]);
+                }
 
                 int color_x = random(generator);
                 int color_y = random(generator);
                 int color_z = random(generator);
-                pcl::transformPointCloud(*source, *transformed_clouds[bucket],
-                                         transform_matrix);
+
                 pcl::visualization::PointCloudColorHandlerCustom<data_t>
-                    handler(transformed_clouds[bucket], color_x, color_y,
-                            color_z);
+                        handler(transformed_clouds[bucket], color_x, color_y,
+                                color_z);
                 viewer.addPointCloud(transformed_clouds[bucket], handler,
-                                     std::string('c', bucket));
-                // pcl::visualization::PointCloudColorHandlerCustom<data_t>
-                //     handler2(source, 240, 240, 240);
-                // viewer.addPointCloud(source, handler2,
-                //                      std::string('x', bucket));
+                                     std::string(bucket, 'c'));
                 pcl::visualization::PointCloudColorHandlerCustom<data_t>
-                    handler3(source, color_x, color_y, color_z);
+                        handler3(source, color_x, color_y, color_z);
                 viewer2.addPointCloud(source, handler3,
-                                      std::string('d', bucket));
+                                      std::string(bucket, 'd'));
             }
             viewer.spin();
             viewer2.spin();
+            // end debug
+#endif
+
+
         }
     };
 }  // namespace psh
